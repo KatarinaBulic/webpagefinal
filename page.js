c988839a2092038f52890b6ac9b0ac34485d8a55
KatarinaBulic <146482326+KatarinaBulic@users.noreply.github.com>
2026-04-24 10:35:36 -0400

    Updated menu page and added new features

--- page.js
+++ page.js
@@ -1,5 +1,14 @@
-if (age > 18) {
-    console.log("Adult");
-} else {
-    console.log("Not adult");
+function showMessage() {
+  alert("Welcome to ByteBite! Start your order below.");
 }
+
+document.getElementById("orderForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let food = document.getElementById("food").value;
+  let quantity = document.getElementById("quantity").value;
+
+  document.getElementById("orderMessage").textContent =
+    "Thank you, " + name + "! Your order for " + quantity + " " + food + "(s) has been received.";
+});
